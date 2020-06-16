3021c4e17b8351161e756e7862b152ac1472af3b
nandinisood <51796675+nandinisood@users.noreply.github.com>
2020-06-16 22:22:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<center><h2><b>TERRAFORM</h2></b></center>
+<center><h2><b>Internet of Things </h2></b></center>
 The Internet of things (IoT) is a system of interrelated computing devices, mechanical and digital machines provided with unique identifiers (UIDs) and the ability to transfer data over a network without requiring human-to-human or human-to-computer interaction.
 <br>
 <pre>
