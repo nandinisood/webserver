ad839754eddd6a257dad2c3e253ce28ecf48582b
nandinisood <51796675+nandinisood@users.noreply.github.com>
2020-06-16 19:09:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<center><h2><b>Terraform</h2></b></center>
+<center><h2><b>TERRAFORM</h2></b></center>
 Terraform is an open-source Infrastructure as code software tool. It is created by HashiCorp. It enables users to define and provision a datacenter infrastructure. It uses a high-level configuration language known as Hashicorp Configuration Language.
 <br>
 <pre>
