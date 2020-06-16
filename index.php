b460199f34e94eec4d25981b2b7c95ab04e8f20e
nandinisood <51796675+nandinisood@users.noreply.github.com>
2020-06-16 18:38:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-Bienvenue
+<center><h2><b>Terraform</h2></b></center>
+Terraform is an open-source Infrastructure as code software tool. It is created by HashiCorp. It enables users to define and provision a datacenter infrastructure. It uses a high-level configuration language known as Hashicorp Configuration Language.
 <br>
 <pre>
 <?php
-echo `ifconfig`
+echo `date`
 ?>
 </pre>
